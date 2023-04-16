5790e79832462cb7ea55681da27b6fa665c52956
Or Rodrigez <orrodrigez1@gmail.com>
2023-04-16 15:34:15 +0300

    Add details to the customer page

--- src/components/CustomerPage.js
+++ src/components/CustomerPage.js
@@ -22,7 +22,23 @@ const CustomerPage = () => {
     <div>
       {loading ? (
         <p>Loading...</p>
-      ) : <h1>{customer.name}</h1>}
+      ) : (
+        <div className="customer-page">
+          <h1>Customer: {customer.name}</h1>
+          <p>Email: {customer.email}</p>
+          <p>Phone: {customer.phone}</p>
+          <p>Status: {customer.status}</p>
+          <p>Last modified: {customer.lastChange}</p>
+          <div className="customer-notes">
+            <h2>Notes</h2>
+            {customer.notes.map((note) => (
+              <div className="single-note" key={note.id}>
+                <p>{note.date} - {note.note}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
